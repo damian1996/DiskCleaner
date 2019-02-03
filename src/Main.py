2c66de94fbb60f3821395c72542ea3dc5a135326
damian1996 <damian260@onet.pl>
2019-02-03 22:44:41 +0100

    Changing concept for removing files

--- src/Main.py
+++ src/Main.py
@@ -2,26 +2,27 @@ from DirectoryTree import DirectoryTree
 import unittest, os
 from File import File
 from Disks import Disks
-from Utils import get_files_to_remove, get_directories_to_remove
+from Utils import get_files_to_delete
 
 def get_files_by_level2(gen):
     for file in gen:
         print(file.get_file_info())
-    print()
+        print()
 
 if __name__ == '__main__':
     tree = DirectoryTree("~/Desktop/ml2018-19")
     gen = tree.walklevel('~/Desktop/ml2018-19')
-    get_files_by_level2(gen)
+    #get_files_by_level2(gen)
+    
     #tree.remove_tree_node("/home/damian/Downloads/4B.cpp")
     #tree.remove_directory("/home/damian/Downloads/sort")
-    files_to_remove = get_files_to_remove(tree.get_root())
-    print("FILES TO REMOVE")
-    get_files_by_level2(files_to_remove)
 
-    dirs_to_remove = get_directories_to_remove(tree.get_root())
-    print("DIRS TO REMOVE")
-    get_files_by_level2(dirs_to_remove)
+    print("FILES TO REMOVE")
+    number_files_to_remove = 5
+    in_past = 20
+    mode = 'days'
+    files_to_remove = get_files_to_delete(tree.get_root(), number_files_to_remove, in_past, mode)
+    get_files_by_level2(files_to_remove)
 
     d = Disks()
     print(d.get_partitions())
--- src/Utils.py
+++ src/Utils.py
@@ -1,4 +1,4 @@
-import time, os
+import datetime, time, os
 from copy import deepcopy
 
 def convert_epoch_time_to_date(epoch_time):
@@ -22,32 +22,27 @@ def sort_files_by_size(files, desc=False):
     files_to_sort.sort(key=f, reverse=desc)
     return files_to_sort
 
-def get_propositions_to_remove(files, no_oldest, no_largest):
-    sorted_files_by_date = sort_files_by_opening_date(files=files, desc=True)
-    oldest_files = sorted_files_by_date[:no_oldest]
-    sorted_files_by_size = sort_files_by_size(files=oldest_files, desc=True)
-    return sorted_files_by_size[:no_largest]
+def get_files_fulfilling_condition(node, predicate, date_bound, curr_time, mode, all_files):
+    last_opened_in_sec = node.get_last_opened_date_in_seconds()
+    last_opened_time = datetime.datetime.utcfromtimestamp(last_opened_in_sec)
+    if mode == 'days':
+        last_opened_time = last_opened_time + datetime.timedelta(days=+date_bound)
+    elif mode == 'month':
+        last_opened_time = last_opened_time + relativedelta(months=+date_bound)
+    
+    if not node.if_is_file():
+        for child in node.get_children():
+            get_files_fulfilling_condition(child, predicate, date_bound, curr_time, mode, all_files)
+    elif predicate(last_opened_time, curr_time):
+        all_files.append(node)
 
-def files_filtering(files, is_file):
-    return [f for f in files if f.is_file == is_file]
-
-def get_all_files_from_tree(node, all_files):
-    all_files.append(node)
-    for child in node.get_children():
-        get_all_files_from_tree(child, all_files)
-
-def get_files_to_remove(root):
+def get_files_to_delete(root, number_files_to_remove, date_bound, mode):
     all_files = []
-    get_all_files_from_tree(root, all_files)
-    only_files = files_filtering(all_files, is_file=True)
-    no_oldest = len(only_files) // 5
-    no_largest = no_oldest // 3
-    return get_propositions_to_remove(only_files, no_oldest, no_largest)
-
-def get_directories_to_remove(root):
-    all_files = []
-    get_all_files_from_tree(root, all_files)
-    only_dirs = files_filtering(all_files, is_file=False)
-    no_oldest = len(only_dirs) // 5
-    no_largest = no_oldest // 3
-    return get_propositions_to_remove(only_dirs, no_oldest, no_largest)
+    curr_time = datetime.datetime.now()
+    predicate = lambda node_time, curr_time: node_time > curr_time
+    get_files_fulfilling_condition(root, predicate, date_bound, curr_time, mode, all_files)
+    if len(all_files) < number_files_to_remove:
+        return all_files
+    else:
+        files_sorted_by_size = sort_files_by_size(all_files, desc=True)
+        return files_sorted_by_size[:number_files_to_remove]
