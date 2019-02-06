1e3954421115d79eb420970a8c5a235350172ccc
damian1996 <damian260@onet.pl>
2019-02-06 02:56:31 +0100

    Main PEP8

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,4 @@
 import kivy
-kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
@@ -7,13 +6,17 @@ from kivy.uix.screenmanager import ScreenManager
 from PartitionsScreen import PartitionsScreen
 from FilesScreen import FilesScreen
 
+kivy.require('1.10.1')
+
 screen_manager = ScreenManager()
 screen_manager.add_widget(PartitionsScreen(name="partitions"))
 screen_manager.add_widget(FilesScreen(name="files"))
 
+
 class DiskCleanerApp(App):
     def build(self):
         return screen_manager
+
 
 if __name__ == '__main__':
     DiskCleanerApp().run()
