2ff4c0f59084dc6a03180db8ae4c0d51124b49cb
damian1996 <damian260@onet.pl>
2019-02-06 02:54:15 +0100

    LoadingWidget PEP8

--- src/LoadingWidget.py
+++ src/LoadingWidget.py
@@ -5,13 +5,15 @@ from kivy.animation import Animation
 from kivy.properties import NumericProperty
 from kivy.graphics import *
 
+
 class LoadingWidget(FloatLayout):
     angle = NumericProperty(0)
+
     def __init__(self, diameter=150, **kwargs):
         super(LoadingWidget, self).__init__(**kwargs)
         self.diameter = diameter
-        anim = Animation(angle = 360, duration=1) 
-        anim += Animation(angle = 360, duration=1)
+        anim = Animation(angle=360, duration=1)
+        anim += Animation(angle=360, duration=1)
         anim.repeat = True
         anim.start(self)
         self.draw(0)
@@ -20,10 +22,12 @@ class LoadingWidget(FloatLayout):
         if angle == 360:
             item.angle = 0
         self.draw(angle)
-    
+
     def draw(self, angle):
         with self.canvas:
             self.canvas.clear()
             Color(1., 1, 1)
             w, h = self.size
-            SmoothLine(ellipse=(w/2-self.diameter/2, h/2-self.diameter/2, self.diameter, self.diameter, angle, angle+90), width=10)
+            SmoothLine(ellipse=(
+                w/2-self.diameter/2, h/2-self.diameter/2,
+                self.diameter, self.diameter, angle, angle+90), width=10)
